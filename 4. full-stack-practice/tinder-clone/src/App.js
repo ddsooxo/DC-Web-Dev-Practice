78b01fcfcecea5f52f7a554d9c2477786cdab845
ddsooxo <dchuteng@gmail.com>
2021-11-14 23:23:36 -0800

    started a full-stack tutorial on a Tinder app clone

---
+++ 4. full-stack-practice/tinder-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // BEM class naming convention for styling. 
+    <div className="app">
+      <h1>MERN TINDER clone </h1>
+
+    {/* Header */}
+    {/* TinderCards */}
+    {/* SwipeButtons */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 4. full-stack-practice/tinder-clone/src/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.css"
+function header() {
+    return (
+        <div className="header">
+            
+        </div>
+    )
+}
+
+export default header
